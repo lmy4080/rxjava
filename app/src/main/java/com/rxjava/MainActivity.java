1d6e8c540258d58d934c7e6b58bb0b7c662f4723
lmy4080 <alsdudwlwhs1@naver.com>
2019-11-01 22:51:50 +0900

    Initial commit

---
+++ app/src/main/java/com/rxjava/MainActivity.java
@@ -0,0 +1,77 @@
+package com.rxjava;
+
+/**
+ * MainActivity
+ * 1. Uses RxJava to handle UI tasks in the foreground and the rest in the background.
+ */
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+
+public class MainActivity extends AppCompatActivity {
+
+    // tag
+    private static final String TAG = "MainActivity";
+
+    // ui
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+
+        // Create Observe
+        Observable<Task> taskObservable = Observable
+                .fromIterable(DataSource.createTasksList())
+                .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Exception {
+                        Log.d(TAG, "test: " + Thread.currentThread().getName());
+                        try {
+                            Thread.sleep(1000); // sleep on the background thread, doesn't affect the UI work.
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return task.isComplete();
+                    }
+                })
+                .observeOn(AndroidSchedulers.mainThread());
+
+        // Subscribe
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe: called.");
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Log.d(TAG, "onNext: " + Thread.currentThread().getName());
+                Log.d(TAG, "onNext: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "onError: ", e);
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "onComplete: called.");
+            }
+        });
+    }
+}
